fd69e0c2a7671730548a0f7eaad796003c09e516
Damian Dawidowski <damiandawidowski@gmail.com>
2023-02-14 21:32:34 +0100

    modified:   js/task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ulList = document.querySelector("#ingredients");
+for (const ingredient of ingredients) {
+  let liElement = document.createElement("li");
+  liElement.textContent = ingredient;
+  liElement.classList.add("item");
+  ulList.append(liElement);
+}
