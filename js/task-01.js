a7cb902e8ecdfc82026817185cf0073ac550a577
Damian Dawidowski <damiandawidowski@gmail.com>
2023-02-14 21:15:34 +0100

    modified:   js/task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,13 @@
+const primaryUlList = document.querySelector("#categories");
+const primaryUlListElements = primaryUlList.children;
+const primaryUlListCount = primaryUlListElements.length;
+console.log(`Number of categories: ${primaryUlListCount}`);
+
+const primaryUlListElementArray = Array.from(primaryUlListElements);
+for (const li of primaryUlListElementArray) {
+  let h2Text = li.querySelector("h2").textContent;
+  console.log(`Category: ${h2Text}`);
+  let innerUlList = li.querySelector("ul");
+  let innerUlListCount = Array.from(innerUlList.children).length;
+  console.log(`Elements: ${innerUlListCount}`);
+}
