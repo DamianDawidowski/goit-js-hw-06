222d67a362d1760352cbb9941ffa35f1060aea40
Damian Dawidowski <damiandawidowski@gmail.com>
2023-02-15 20:43:24 +0100

    modified:   js/task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,11 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const Btn = document.querySelector("button");
+const Body = document.querySelector("body");
+const SpanColorvalue = document.querySelector("span");
+Btn.addEventListener("click", (event) => {
+  let newColor = getRandomHexColor();
+  Body.style.backgroundColor = newColor;
+  SpanColorvalue.textContent = newColor;
+});
