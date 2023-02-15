a74fd859df115d4dab91723aea821ab8343edb7f
Damian Dawidowski <damiandawidowski@gmail.com>
2023-02-15 17:54:26 +0100

    modified:   js/task-04.js

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+let counterValue = 0;
+ 
+let referenceArea = document.querySelector("#value");
+const decrementBtn = referenceArea.previousElementSibling;  
+const incrementBtn = referenceArea.nextElementSibling; 
+referenceArea.textContent = `${counterValue}`; 
+
+ console.log(referenceArea.textContent);
+ 
+const increaseByClick = (event) => {
+counterValue++; 
+referenceArea.textContent = `${counterValue}`; 
+};
+
+const decreaseByClick = (event2) => {
+counterValue--; 
+referenceArea.textContent = `${counterValue}`; 
+};
+ 
+decrementBtn.addEventListener("click", decreaseByClick);
+incrementBtn.addEventListener("click", increaseByClick);
